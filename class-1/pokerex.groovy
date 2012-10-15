060eb7cba9f973e0708e15a3a8fcdfbc4848874c
bbk-pij-2012-15 <hdavey01@mail.bbk.ac.uk>
2012-10-15 14:28:58 +0200

    Update class-1/pokerex.groovy
    finished validity checks; put in useful comment breaks

--- class-1/pokerex.groovy
+++ class-1/pokerex.groovy
@@ -1,15 +1,13 @@
-array validRanks = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]
-array validSuits = [ "H", "D", "C", "S" ]
+validRanks = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]
+validSuits = [ "H", "D", "C", "S" ]
 String card1, card2, card3, card4, card5 = null
 String unverifiedRank, unverifiedSuit, unverifiedCard, validRank, validSuit
-int count = 0
-
-while (card5 ==null)
-{
-count = count + 1
+int count = 1
 boolean suit, rank = false
 
-print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C: "
+/////////////////////////////////////////////////////VALIDITY CHECKS START/////////////////////////////////////////////////////////////
+
+print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C, Ace of Spades AS: "
 unverifiedCard = System.console().readLine()
 unverifiedRank = unverifiedCard.substring(0,1)
 unverifiedSuit = unverifiedCard.substring(1,2)
@@ -20,8 +18,7 @@ if (validRanks.contains(unverifiedRank))
     }
 else 
     {
-    print "invalid rank. input must be in range [12345678910JQKA]"
-    count = count - 1
+    print "invalid rank. input must be in range [12345678910JQKA]. Quit and start again"
     }
 if (validSuits.contains(unverifiedSuit))
     {  
@@ -30,10 +27,131 @@ if (validSuits.contains(unverifiedSuit))
     }
 else 
     {
-    print "invalid suit. input must be in range [HDCS]"
-    count = count - 1
+    print "invalid suit. input must be in range [HDCS]. Quit and start again"
     }
 
-    if (suit = true && rank = true)
+    if (suit == true && rank == true)
         {
-         card = "$validSuit$validRank"
+         card1 = "$validRank$validSuit"
+         count = count + 1
+        }
+        
+print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C, Ace of Spades AS: "
+unverifiedCard = System.console().readLine()
+unverifiedRank = unverifiedCard.substring(0,1)
+unverifiedSuit = unverifiedCard.substring(1,2)
+if (validRanks.contains(unverifiedRank))
+    {  
+    validRank = unverifiedRank
+    rank = true
+    }
+else 
+    {
+    print "invalid rank. input must be in range [12345678910JQKA]. Quit and start again"
+    }
+if (validSuits.contains(unverifiedSuit))
+    {  
+    validSuit = unverifiedSuit
+    suit = true
+    }
+else 
+    {
+    print "invalid suit. input must be in range [HDCS]. Quit and start again"
+    }
+
+    if (suit == true && rank == true)
+        {
+         card2 = "$validRank$validSuit"
+         count = count + 1
+        }
+        
+print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C, Ace of Spades AS: "
+unverifiedCard = System.console().readLine()
+unverifiedRank = unverifiedCard.substring(0,1)
+unverifiedSuit = unverifiedCard.substring(1,2)
+if (validRanks.contains(unverifiedRank))
+    {  
+    validRank = unverifiedRank
+    rank = true
+    }
+else 
+    {
+    print "invalid rank. input must be in range [12345678910JQKA]. Quit and start again"
+    }
+if (validSuits.contains(unverifiedSuit))
+    {  
+    validSuit = unverifiedSuit
+    suit = true
+    }
+else 
+    {
+    print "invalid suit. input must be in range [HDCS]. Quit and start again"
+    }
+
+    if (suit == true && rank == true)
+        {
+         card3 = "$validRank$validSuit"
+         count = count + 1
+        }
+        
+print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C, Ace of Spades AS: "
+unverifiedCard = System.console().readLine()
+unverifiedRank = unverifiedCard.substring(0,1)
+unverifiedSuit = unverifiedCard.substring(1,2)
+if (validRanks.contains(unverifiedRank))
+    {  
+    validRank = unverifiedRank
+    rank = true
+    }
+else 
+    {
+    print "invalid rank. input must be in range [12345678910JQKA]. Quit and start again"
+    }
+if (validSuits.contains(unverifiedSuit))
+    {  
+    validSuit = unverifiedSuit
+    suit = true
+    }
+else 
+    {
+    print "invalid suit. input must be in range [HDCS]. Quit and start again"
+    }
+
+    if (suit == true && rank == true)
+        {
+         card4 = "$validRank$validSuit"
+         count = count + 1
+        }
+        
+print "Please enter card $count in format (XY), where X is Rank and Y is Suit. Two of Clubs would be 2C, Ace of Spades AS: "
+unverifiedCard = System.console().readLine()
+unverifiedRank = unverifiedCard.substring(0,1)
+unverifiedSuit = unverifiedCard.substring(1,2)
+if (validRanks.contains(unverifiedRank))
+    {  
+    validRank = unverifiedRank
+    rank = true
+    }
+else 
+    {
+    print "invalid rank. input must be in range [12345678910JQKA]. Quit and start again"
+    }
+if (validSuits.contains(unverifiedSuit))
+    {  
+    validSuit = unverifiedSuit
+    suit = true
+    }
+else 
+    {
+    print "invalid suit. input must be in range [HDCS]. Quit and start again"
+    }
+
+    if (suit == true && rank == true)
+        {
+         card5 = "$validRank$validSuit"
+         count = count + 1
+        }
+        
+////////////////////////////////////////////////////////////VALIDITY CHECKS END/////////////////////////////////////////////////////////
+
+print "\t$card1\t$card2\t$card3\t$card4\t$card5"
