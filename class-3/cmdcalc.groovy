4b522a49e715809b39209f36c2d7c3b324cdf60c
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2012-10-15 16:48:12 +0100

    added command to strip the whitespace

--- class-3/cmdcalc.groovy
+++ class-3/cmdcalc.groovy
@@ -2,6 +2,7 @@ print "command line calculator v0.1"
 print "\nPlease enter a sum consisting of two number and an operator, followed by <ENTER>"
 print "\n(operators must be +, -, * or /)"
 String sum = System.console().readLine()
+sum = sum.replaceAll("\\s","")
 boolean add, subtract, multiply, divide = false
 
 if (sum.contains("+"))
@@ -24,6 +25,9 @@ if (sum.contains("/"))
 	divide = true
 	int operatorPosition = sum.indexOf("/")
 }
+
+double number2 = sum.substring(operatorPosition)
+
 
 int sumSize = sum.size()
 print sumSize
