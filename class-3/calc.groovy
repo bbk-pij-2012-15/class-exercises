f57cfb7dbb6002147bac48d47a00cab1bba520ee
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2012-10-15 15:25:06 +0100

    first commit

---
+++ class-3/calc.groovy
@@ -0,0 +1,32 @@
+println "calculator v0.1"
+print "Please enter two numbers, separated by pressing <ENTER>"
+String inputOne = System.console().readLine()
+String inputTwo = System.console().readLine()
+double numberOne = Double.parseDouble(inputOne)
+double numberTwo = Double.parseIDouble(inputTwo)
+
+print "Please select an operation by typing its number and prerssing <ENTER>:"
+print "\n1 = add\n2 = subtract\n3 = divide\n 4 = multiply"
+String choice = System.console().readLine()
+int operation = Integer.parseInt(choice)
+
+if (operation == 1)
+{
+	double answer = (numberOne + numberTwo)
+}
+else if (operation == 2)
+{
+	double answer = (numberOne - numberTwo)
+}
+else if (operation == 3)
+{
+	double answer = (numberOne / numberTwo)
+}
+else if (operation == 4)
+{
+	double answer = (numberOne * numberTwo)
+}
+else
+{
+	print "\ninvalid input. please start over and choose an operation from the range [1234]"
+}
