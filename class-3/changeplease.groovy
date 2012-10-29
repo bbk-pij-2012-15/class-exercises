16d734905ca1efaaea103d0374b3928041e08a82
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2012-10-29 15:06:08 +0000

    completely changed program; added variable $special, a changing divisor

--- class-3/changeplease.groovy
+++ class-3/changeplease.groovy
@@ -1,21 +1,44 @@
-double cost, paid, change, temp, newChange
-int fiftyNote, twentyNote, tenNote, fiveNote, twopoundCoin, onepoundCoin, fiftyCoin, twentyCoin, tenCoin, fiveCoin, twoCoin, oneCoin
+double cost, paid, change, temp, newChange, changeRemaining
+int loopNumber = 0, amount_of_note_or_coin, special
+String note_or_coin_name
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 print "Please enter item cost: "
 itemCost = System.console().readLine()
-cost = Integer.parseInt(itemCost)
+cost = Double.parseDouble(itemCost)
 print "\nPlease enter amount paid: "
 amountPaid = System.console().readLine()
-paid = Integer.parseInt(amountPaid)
+paid = Double.parseDouble(amountPaid)
 changeRemaining = (paid - cost)
+change = (paid - cost)
+print "Your change is: $change"
 
-fiftyNote = (changeRemaining / 50)
-newChange = 50 * fiftyNote
-changeRemaining = (changeRemaining - newChange)
+while (changeRemaining > 0)
+{
+    loopNumber++
+    if (loopNumber == 1) {
+        special = 50 ; 
+        note_or_coin_name = "£50 notes"
+    } else if (loopNumber == 2) {special = 20 ; note_or_coin_name = "£20 notes"}
+    else if (loopNumber == 3) {special = 10 ; note_or_coin_name = "£10 notes"}
+    else if (loopNumber == 4) {special = 5 ; note_or_coin_name = "£5 notes"}
+    else if (loopNumber == 5) {special = 2 ; note_or_coin_name = "£2 coins"}
+    else if (loopNumber == 6) {special = 1 ; note_or_coin_name = "£1 coins"}
+    else if (loopNumber == 7) {special = 0.50 ; note_or_coin_name = "50p coins"}
+    else if (loopNumber == 8) {special = 0.20 ; note_or_coin_name = "20p coins"}
+    else if (loopNumber == 9) {special = 0.10 ; note_or_coin_name = "10p coins"}
+    else if (loopNumber == 10) {special = 0.05 ; note_or_coin_name = "5p coins"}
+    else if (loopNumber == 11) {special = 0.02 ; note_or_coin_name = "2p coins"}
+    else if (loopNumber == 12) {special = 0.01 ; note_or_coin_name = "1p coins"}
+    else if (loopNumber > 12) {
+        break
+    }
 
-twentyNote = (changeRemaining / 20)
-newChange = 20 * twentyNote
-changeRemaining = (changeRemaining - newChange)
+    amount_of_note_or_coin = (changeRemaining / special)
+    newChange = (special * amount_of_note_or_coin)
+    changeRemaining = (changeRemaining - newChange)
 
-
-
-print "$fiftyNote\n$newChange\n$changeRemaining\n$twentyNote"
+    print "\n You should receive: $amount_of_note_or_coin $note_or_coin_name"
+    println "\n amount_of_note_or_coin = $amount_of_note_or_coin \n changeRemaining = $changeRemaining \n special = $special"
+   
+    print "\n$changeRemaining"
+}
